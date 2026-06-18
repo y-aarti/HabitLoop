9125f50c83c1211004c639b62b38df1726eb1285
webnaari <y.aarti.official@gmail.com>
2026-06-18 18:16:08 +0530

    Add frontend CI/CD

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 
-const API_URL = "http://localhost:3000/questions";
+const BASE_URL = import.meta.env.VITE_API_URL;
+console.log('base',import.meta.env.VITE_API_URL);
+const API_URL = `${BASE_URL}/questions`;
 
 function App() {
   const [questions, setQuestions] = useState([]);
